9f4257054cc2bf1c240e0bca24a1b53c15ecc743
Kyle Payne <kjp44@njit.edu>
2019-03-12 16:57:42 -0400

    Fixed parsing to remove javascript

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ search.changeDirectory('Pages')
 fileNames = search.getFileNames()
 
 pageContent = search.getPageContent(fileNames[0])
+
+soup = search.getSoup(pageContent)
+
+pageText = search.getPageText(soup)
+
+print(pageText)
--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 import os
+from bs4 import BeautifulSoup
+
 
 def changeDirectory(path):
     os.chdir(path)
@@ -10,3 +12,14 @@ def getFileNames():
 def getPageContent(fileName):
 	f = open(fileName, 'r', encoding='utf-8', errors='ignore')
 	return f.read()
+
+def getSoup(pageContent):
+	soup = BeautifulSoup(pageContent, 'html.parser')
+	for script in soup(["script", "style"]):
+		script.decompose()
+	return soup
+
+
+def getPageText(soup):
+    pageText = soup.get_text()
+    return pageText
